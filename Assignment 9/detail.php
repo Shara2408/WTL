ff8cad08768c0a25dd3db70f3125b17d16a40161
Shara2408 <kumbharshravani2408@gmail.com>
2025-05-05 14:54:42 +0530

    Add files via upload

---
+++ Assignment 9/detail.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>My Details</h2>";
+$name = "SHARA";
+$age = 21;
+$course = "Computer Science";
+$hobby = "Reading";
+
+echo "Name: $name <br>";
+echo "Age: $age <br>";
+echo "Course: $course <br>";
+echo "Hobby: $hobby <br>";
+?>
+
+</body>
+</html>
