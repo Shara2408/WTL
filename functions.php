f5ea4b861b8f23ee3615aa9ce19201414daa6899
Shara2408 <kumbharshravani2408@gmail.com>
2025-05-09 12:23:38 +0530

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>Function Program</h2>";
+
+function greetUser($name) {
+    return "Hello, $name!";
+}
+
+echo greetUser("SHARA") . "<br><br>";
+?>
+
+
+</body>
+</html>
